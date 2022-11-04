0fc86c51a9a8f63fbf4c246263c225ec648d87df
muslehuddinnayeem <94736334+muslehuddinnayeem@users.noreply.github.com>
2022-11-04 18:57:06 +0600

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+<script>
+        var btn = document.getElementById("btn");
+        var light = document.getElementById("light");
+
+        function toogleBtn(){
+            btn.classList.toogle("active");
+            light.classList.toogle("on");
+        }
+
+    </script>
